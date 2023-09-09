f4239d02d7161ad2bfef8b37a3a59dc18539d67c
Misha <mishagrigoryan@gmail.com>
2023-09-08 23:49:31 -0400

    sqlite

--- rando,.py
+++ rando,.py
@@ -1,3 +1,9 @@
 import pandas as pd
 data = pd.read_csv('file.csv')
 data.to_csv('output.csv', index=False)
+import pandas as pd
+import sqlite3
+
+connection = sqlite3.connect('database.db')
+data = pd.read_sql_query('SELECT * FROM table_name', connection)
+data.to_sql('table_name', connection, if_exists='replace', index=False)
